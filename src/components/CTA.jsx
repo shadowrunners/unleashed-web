2bce9b640bd1d2afdbe5f69065f0c638b4870560
scrappie <scr3ppie@gmail.com>
2023-05-01 23:50:59 +0300

    chore: updated CTA

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -8,13 +8,12 @@ const CTA = () => (
     <div className="flex-1 flex flex-col">
       <h2 className={styles.heading2}>So, what are you waiting for?</h2>
       <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
-        We've already shown you what Evelyn is capable of. Your choice now, pal.
+        We've shown you what Evelyn is capable of. Your choice now.
       </p>
     </div>
     <div className={`${styles.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
       <Button />
     </div>
-    .
   </section>
 );
 
