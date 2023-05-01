800677a7a7b7727876bae18d4a6d5f7b0e869226
scrappie <scr3ppie@gmail.com>
2023-05-01 23:53:25 +0300

    fix: stats now render properly on mobile

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -29,63 +29,81 @@ const Stats = () => {
     //        });
     // }, []);
 
-  return (
-    <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
-      <div className="flex justify-center items-center w-full mb-6">
-        <motion.h4
+    return (
+      <section id="section" className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+        <div className="flex justify-center items-center w-full mb-6 flex-col sm:flex-row">
+          <motion.h4
             ref={ref}
             initial={{ opacity: 0 }}
             animate={{ opacity: inView ? 1 : 0 }}
             transition={{ delay: 0.6 }}
-            className={`font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white`}
-        >
-          We're currently serving
-        </motion.h4>
-
-        <div className="flex justify-start items-center flex-row m-3">
-          <motion.h4
-            ref={ref}
-            className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] text-gradient leading-[43px] text-white"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: inView ? 1 : 0 }}
-            transition={{ delay: 1.0 }}
+            className={`font-poppins font-semibold xs:text-[40px] text-[30px] text-white xs:leading-[53px] leading-[43px] sm:ml-5 text-center`}
           >
-           <Counter value={32} />
+            We're currently serving
           </motion.h4>
-          <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-2">
-            Servers
-          </p>
-        </div>
+  
+          <div className="flex justify-start items-center m-3">
+            <motion.h4
+              ref={ref}
+              className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] text-gradient leading-[43px] text-white"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{ delay: 1.0 }}
+            >
+             <Counter value={32} />
+            </motion.h4>
+  
+            <motion.h4
+              ref={ref}
+              className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] text-gradient leading-[43px] text-white"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{ delay: 1.0 }}
+            >
+             <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-2">
+              Servers
+            </p>
+            </motion.h4>
+          </div>
 
-        <div className="flex justify-start items-center flex-row ml-3">
-        <motion.h4
-            ref={ref}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: inView ? 1 : 0 }}
-            transition={{ delay: 1.0 }}
-            className={`font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white`}
-        >
-          and
-        </motion.h4>
+          <div className="flex justify-start items-center ml-3 mr-2">
+            <motion.h4
+              ref={ref}
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{ delay: 1.0 }}
+              className={`font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white`}
+            >
+              and
+            </motion.h4>
+          </div>
+  
+          <div className="flex justify-start items-center m-3 sm:flex">
+            <motion.h4
+              ref={ref}
+              className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] text-gradient leading-[43px] text-white"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{ delay: 1.0 }}
+            >
+             <Counter value={1802} />
+            </motion.h4>
+  
+            <motion.h4
+              ref={ref}
+              className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] text-gradient leading-[43px] text-white"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: inView ? 1 : 0 }}
+              transition={{ delay: 1.0 }}
+            >
+             <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-2">
+              Users
+            </p>
+            </motion.h4>
+          </div>
         </div>
-
-        <div className="flex justify-start items-center flex-row m-3">
-          <motion.h4
-            ref={ref}
-            className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] text-gradient leading-[43px] text-white"
-            initial={{ opacity: 0 }}
-            animate={{ opacity: inView ? 1 : 0 }}
-            transition={{ delay: 1.0 }}
-          >
-           <Counter value={1802} />
-          </motion.h4>
-          <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-2">
-            Users
-          </p>
-        </div>
-      </div>
-    </section>
-  );
+      </section>
+    );
 };
 
 export default Stats;
